8aa88db6e21b0f540a446adfe5c8f6e0b31c3713
Gabriel <gabrieljcoutinho@gmail.com>
2025-04-25 11:25:29 -0300

    mudando a url

--- src/App.js
+++ src/App.js
@@ -51,26 +51,26 @@ function App() {
         <Route path="/Menu" element={<Menu />} /> {/* Rota para qualquer URL não correspondente */}
 
         {/*Links shop */}
-        <Route path="/loja/rolinhoPrimavera" element={<RolinhoPrimavera />} />
-        <Route path="/loja/ceviche" element={<Ceviche />} />
-        <Route path="/loja/guioza" element={<Guioza />} />
-        <Route path="/loja/hotRoll" element={<HotRollPage />} />
-        <Route path="/loja/kare" element={<Kare />} />
-        <Route path="/loja/lamen" element={<Lamen />} />
-        <Route path="/loja/missoshiru" element={<Missoshiru />} />
-        <Route path="/loja/niguiriCamarao" element={<NiguiriCamarao />} />
-        <Route path="/loja/salmao" element={<Salmao />} />
-        <Route path="/loja/sashimiPeixeBranco" element={<SashimiPeixeBranco />} />
-        <Route path="/loja/Shimeji" element={<Shimeji />} />
-        <Route path="/loja/Sunomomo" element={<Sunomomo />} />
-        <Route path="/loja/TemakiSalmao" element={<TemakiSalmao />} />
-        <Route path="/loja/Tempura" element={<Tempura />} />
-        <Route path="/loja/Yakisoba" element={<Yakisoba />} />
-        <Route path="/loja/YakisobaFrutosDoMar" element={<YakisobaFrutosDoMar />} />
-        <Route path="/loja/Hossomaki" element={<Hossomaki />} />
-        <Route path="/loja/Yakitori" element={<Yakitori />} />
-        <Route path="/loja/LulaEmpanada" element={<LulaEmpanada />} />
-        <Route path="/loja/Teriaki" element={<Teriaki />} />
+        <Route path="/loja/rolinhoPrimavera/Compra/Delivery/Loja/Oficial" element={<RolinhoPrimavera />} />
+        <Route path="/loja/ceviche/Compra/Delivery/Loja/Oficial" element={<Ceviche />} />
+        <Route path="/loja/guioza/Compra/Delivery/Loja/Oficial" element={<Guioza />} />
+        <Route path="/loja/hotRoll/Compra/Delivery/Loja/Oficial" element={<HotRollPage />} />
+        <Route path="/loja/kare/Compra/Delivery/Loja/Oficial" element={<Kare />} />
+        <Route path="/loja/lamen/Compra/Delivery/Loja/Oficial" element={<Lamen />} />
+        <Route path="/loja/missoshiru/Compra/Delivery/Loja/Oficial" element={<Missoshiru />} />
+        <Route path="/loja/niguiriCamarao/Compra/Delivery/Loja/Oficial" element={<NiguiriCamarao />} />
+        <Route path="/loja/salmao/Compra/Delivery/Loja/Oficial" element={<Salmao />} />
+        <Route path="/loja/sashimiPeixeBranco/Compra/Delivery/Loja/Oficial" element={<SashimiPeixeBranco />} />
+        <Route path="/loja/Shimeji/Compra/Delivery/Loja/Oficial" element={<Shimeji />} />
+        <Route path="/loja/Sunomomo/Compra/Delivery/Loja/Oficial" element={<Sunomomo />} />
+        <Route path="/loja/TemakiSalmaov" element={<TemakiSalmao />} />
+        <Route path="/loja/Tempura/Compra/Delivery/Loja/Oficial" element={<Tempura />} />
+        <Route path="/loja/Yakisoba/Compra/Delivery/Loja/Oficial" element={<Yakisoba />} />
+        <Route path="/loja/YakisobaFrutosDoMar/Compra/Delivery/Loja/Oficial" element={<YakisobaFrutosDoMar />} />
+        <Route path="/loja/Hossomaki/Compra/Delivery/Loja/Oficial" element={<Hossomaki />} />
+        <Route path="/loja/Yakitori/Compra/Delivery/Loja/Oficial" element={<Yakitori />} />
+        <Route path="/loja/LulaEmpanada/Compra/Delivery/Loja/Oficial" element={<LulaEmpanada />} />
+        <Route path="/loja/Teriaki/Compra/Delivery/Loja/Oficial" element={<Teriaki />} />
 
 
 
--- src/routes/Loja.js
+++ src/routes/Loja.js
@@ -31,7 +31,7 @@ const pratosInfo = [
     descricao: 'Massa fina e crocante recheada com queijo muçarela derretido.',
     ingredientes: 'Massa de harumaki, queijo muçarela.',
     preco: 'R$ 14,00',
-    link: '/loja/rolinhoPrimavera' // Link para a página do Rolinho Primavera - MODIFICADO!
+    link: '/loja/rolinhoPrimavera/Compra/Delivery/Loja/Oficial' // Link para a página do Rolinho Primavera - MODIFICADO!
   },
   {
     nome: 'Ceviche',
@@ -39,7 +39,7 @@ const pratosInfo = [
     descricao: 'Peixe branco marinado em suco cítrico com cebola roxa, coentro e pimenta.',
     ingredientes: 'Peixe branco fresco, limão, cebola roxa, coentro, pimenta dedo de moça.',
     preco: 'R$ 30,00',
-    link: '/loja/ceviche' // Link para a página do Ceviche
+    link: '/loja/ceviche/Compra/Delivery/Loja/Oficial' // Link para a página do Ceviche
   },
   {
     nome: 'Guioza',
@@ -47,7 +47,7 @@ const pratosInfo = [
     descricao: 'Pastelzinho japonês recheado com carne suína e legumes, cozido no vapor e depois dourado.',
     ingredientes: 'Massa de guioza, carne suína moída, repolho, cebolinha, gengibre, alho, shoyu, óleo de gergelim.',
     preco: 'R$ 16,00',
-    link: '/loja/guioza' // Link para a página do Guioza
+    link: '/loja/guioza/Compra/Delivery/Loja/Oficial' // Link para a página do Guioza
   },
   {
     nome: 'Hot Roll',
@@ -55,7 +55,7 @@ const pratosInfo = [
     descricao: 'Sushi enrolado em alga nori, recheado com salmão e cream cheese, empanado e frito.',
     ingredientes: 'Arroz japonês, alga nori, salmão, cream cheese, farinha de trigo, ovo, farinha panko.',
     preco: 'R$ 20,00',
-    link: '/loja/hotRoll' // Link para a página do Hot Roll
+    link: '/loja/hotRoll/Compra/Delivery/Loja/Oficial' // Link para a página do Hot Roll
   },
   {
     nome: 'Kare',
@@ -63,7 +63,7 @@ const pratosInfo = [
     descricao: 'Ensopado japonês com carne, batata, cenoura e um rico molho curry.',
     ingredientes: 'Carne bovina em cubos, batata, cenoura, cebola, molho curry japonês, caldo de carne.',
     preco: 'R$ 35,00',
-    link: '/loja/kare' // Link para a página do Kare
+    link: '/loja/kare/Compra/Delivery/Loja/Oficial' // Link para a página do Kare
   },
   {
     nome: 'Lamen',
@@ -71,7 +71,7 @@ const pratosInfo = [
     descricao: 'Sopa de macarrão japonesa com caldo saboroso, carne de porco, ovo cozido, cebolinha e alga nori.',
     ingredientes: 'Macarrão lamen, caldo de porco, carne de porco (chashu), ovo cozido, cebolinha picada, alga nori.',
     preco: 'R$ 28,00',
-    link: '/loja/lamen' // Link para a página do Lamen
+    link: '/loja/lamen/Compra/Delivery/Loja/Oficial' // Link para a página do Lamen
   },
   {
     nome: 'Missoshiru',
@@ -79,7 +79,7 @@ const pratosInfo = [
     descricao: 'Sopa japonesa feita com pasta de soja fermentada (missô) e tofu.',
     ingredientes: 'Missô, tofu em cubos, alga wakame, cebolinha picada, caldo dashi.',
     preco: 'R$ 12,00',
-    link: '/loja/missoshiru' // Link para a página do Missoshiru
+    link: '/loja/missoshiru/Compra/Delivery/Loja/Oficial' // Link para a página do Missoshiru
   },
   {
     nome: 'Niguiri de Camarão',
@@ -87,7 +87,7 @@ const pratosInfo = [
     descricao: 'Bolinho de arroz coberto com fatia de camarão cozido.',
     ingredientes: 'Arroz japonês, camarão cozido, wasabi (opcional).',
     preco: 'R$ 5,50',
-    link: '/loja/niguiriCamarao' // Link para a página do Niguiri de Camarão
+    link: '/loja/niguiriCamarao/Compra/Delivery/Loja/Oficial' // Link para a página do Niguiri de Camarão
   },
   {
     nome: 'Salmão',
@@ -95,7 +95,7 @@ const pratosInfo = [
     descricao: 'Fatia fresca de salmão cru.',
     ingredientes: 'Salmão fresco.',
     preco: 'R$ 9,50',
-    link: '/loja/salmao' // Link para a página do Salmão
+    link: '/loja/salmao/Compra/Delivery/Loja/Oficial' // Link para a página do Salmão
   },
   {
     nome: 'Sashimi de Peixe Branco',
@@ -103,7 +103,7 @@ const pratosInfo = [
     descricao: 'Fatias finas de peixe branco cru.',
     ingredientes: 'Peixe branco fresco (tilápia, robalo, etc.).',
     preco: 'R$ 6,00',
-    link: '/loja/sashimiPeixeBranco' // Link para a página do Sashimi de Peixe Branco
+    link: '/loja/sashimiPeixeBranco/Compra/Delivery/Loja/Oficial' // Link para a página do Sashimi de Peixe Branco
   },
   {
     nome: 'Shimeji',
@@ -111,7 +111,7 @@ const pratosInfo = [
     descricao: 'Cogumelos shimeji salteados na manteiga com shoyu e cebolinha.',
     ingredientes: 'Cogumelos shimeji, manteiga, shoyu, cebolinha picada.',
     preco: 'R$ 12,00',
-    link: '/loja/Shimeji' // Link para a página do Shimeji
+    link: '/loja/Shimeji/Compra/Delivery/Loja/Oficial' // Link para a página do Shimeji
   },
   {
     nome: 'Sunomono',
@@ -119,7 +119,7 @@ const pratosInfo = [
     descricao: 'Salada japonesa agridoce de pepino e alga wakame.',
     ingredientes: 'Pepino, alga wakame, vinagre de arroz, açúcar, sal, gergelim.',
     preco: 'R$ 13,50',
-    link: '/loja/Sunomomo' // Link para a página do Sunomono
+    link: '/loja/Sunomomo/Compra/Delivery/Loja/Oficial' // Link para a página do Sunomono
   },
   {
     nome: 'Temaki de Salmão',
@@ -127,7 +127,7 @@ const pratosInfo = [
     descricao: 'Cone de alga nori recheado com arroz, salmão e cream cheese.',
     ingredientes: 'Alga nori, arroz japonês, salmão, cream cheese, cebolinha (opcional).',
     preco: 'R$ 15,75',
-    link: '/loja/TemakiSalmao' // Link para a página do Temaki de Salmão
+    link: '/loja/TemakiSalmao/Compra/Delivery/Loja/Oficial' // Link para a página do Temaki de Salmão
   },
   {
     nome: 'Tempura',
@@ -135,7 +135,7 @@ const pratosInfo = [
     descricao: 'Legumes e camarões empanados em massa leve e crocante.',
     ingredientes: 'Camarões, legumes variados (abóbora, berinjela, brócolis, etc.), farinha de trigo, ovo, água gelada.',
     preco: 'R$ 28,00',
-    link: '/loja/Tempura' // Link para a página do Tempura
+    link: '/loja/Tempura/Compra/Delivery/Loja/Oficial' // Link para a página do Tempura
   },
   {
     nome: 'Yakisoba',
@@ -143,7 +143,7 @@ const pratosInfo = [
     descricao: 'Macarrão japonês frito com carne, legumes e molho especial.',
     ingredientes: 'Macarrão para yakisoba, carne bovina em tiras, repolho, cenoura, brócolis, cebola, molho yakisoba.',
     preco: 'R$ 13,00',
-    link: '/loja/Yakisoba' // Link para a página do Yakisoba
+    link: '/loja/Yakisoba/Compra/Delivery/Loja/Oficial' // Link para a página do Yakisoba
   },
   {
     nome: 'Yakisoba de Frutos do Mar',
@@ -151,7 +151,7 @@ const pratosInfo = [
     descricao: 'Macarrão japonês frito com camarão, lula, polvo e legumes, ao molho especial.',
     ingredientes: 'Macarrão para yakisoba, camarão, lula, polvo, repolho, cenoura, brócolis, cebola, molho yakisoba.',
     preco: 'R$ 13,50',
-    link: '/loja/YakisobaFrutosDoMar' // Link para a página do Yakisoba de Frutos do Mar
+    link: '/loja/YakisobaFrutosDoMar/Compra/Delivery/Loja/Oficial' // Link para a página do Yakisoba de Frutos do Mar
   },
   {
     nome: 'Hossomaki',
@@ -159,7 +159,7 @@ const pratosInfo = [
     descricao: 'Hossomaki é um sushi pequeno e fino. Tem arroz, uma alga marinha por fora e só um recheio dentro, como atum, salmão ou pepino',
     ingredientes: 'Nori (alga marinha), Shari (arroz para sushi), Um único recheio (ex: atum, salmão, pepino)',
     preco: 'R$ 11,50',
-    link: '/loja/Hossomaki' // Link para a página do Hossomaki
+    link: '/loja/Hossomaki/Compra/Delivery/Loja/Oficial' // Link para a página do Hossomaki
   },
   {
     nome:'Yakitori',
@@ -167,15 +167,15 @@ const pratosInfo = [
     descricao: 'Yakitori é um espetinho japonês, geralmente de frango, grelhado e temperado com molho ou sal.',
     ingredientes: 'Pedaços de frango ou outros alimentos espetados em palitos e temperados com molho ou sal',
     preco: 'R$ 13,00',
-    link: '/loja/Yakitori' // Link para a página do Yakitori
+    link: '/loja/Yakitori/Compra/Delivery/Loja/Oficial' // Link para a página do Yakitori
   },
   {
-    nome:'Lula empanada',
+    nome:'Lula empanada/Compra/Delivery/Loja/Oficial',
     imagem: img19,
     descricao: 'Lula empanada é lula cortada em anéis, revestida com uma camada crocante e frita.',
     ingredientes: 'Anéis de lula, farinha de trigo, ovo , temperos (sal, pimenta, páprica, limão)',
     preco: 'R$ 30,00',
-    link: '/loja/LulaEmpanada' // Link para a página da Lula Empanada
+    link: '/loja/LulaEmpanada/Compra/Delivery/Loja/Oficial' // Link para a página da Lula Empanada
   },
   {
     nome:'Frango Teriaki',
@@ -183,7 +183,7 @@ const pratosInfo = [
     descricao: ' prato japonês onde pedaços de frango são grelhados ou assados com um molho doce e salgado à base de shoyu, mirin e açúcar',
     ingredientes: 'frango e um molho feito principalmente de shoyu, mirin e açúcar.',
     preco: 'R$ 35,00',
-    link: '/loja/Teriaki' // Link para a página do Frango Teriyaki
+    link: '/loja/Teriaki/Compra/Delivery/Loja/Oficial' // Link para a página do Frango Teriyaki
   },
 ];
 
@@ -224,8 +224,6 @@ const Loja = () => {
                   href={prato.link}
                   className='btnComprar'
                   title='Clicar'
-                  target="_blank"
-                  rel="noopener noreferrer"
                 >
                   <span>Comprar</span><i></i>
                 </a>
